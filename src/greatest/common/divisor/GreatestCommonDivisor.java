e8c7bad6deb3fe6d2f313180bde7cabb5b0b8a6a
aristeridis <aristeridis@hotmail.gr>
2022-03-29 22:39:25 +0300

    https://code-exercises.com/programming/medium/ final commit

--- src/greatest/common/divisor/GreatestCommonDivisor.java
+++ src/greatest/common/divisor/GreatestCommonDivisor.java
@@ -10,12 +10,38 @@ import java.util.*;
  * @author alexandrosaristeridis
  */
 public class GreatestCommonDivisor {
-
+    public Integer gcd(Integer p, Integer q) {
+        int di = 1;
+        int max=0;
+        if(p>=q){
+            max=p;
+        }else{
+            max=q;
+        }
+        for (int i = 1; i <=max; i++) {
+            if (p%i==0 && q%i==0){
+                di=i;
+            }
+        }
+        return di;
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        GreatestCommonDivisor obj=new GreatestCommonDivisor();
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("dose int1");
+
+        int n=s.nextInt();
+        System.out.println("dose int2");
+
+        int k=s.nextInt();
+
+        
+        System.out.println(obj.gcd(n,k));
     }
     
 }
